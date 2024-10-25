bc2f0598bc0a6b965e01b3958701a3c4fd2dd0ca
Amie <97197239+ARomano79@users.noreply.github.com>
2024-10-24 21:21:04 -0400

    started building site

---
+++ src/soapQs.js
@@ -0,0 +1,26 @@
+
+const genGreeting=["Chief concern?", "How is Pet doing?"];
+
+const sysQs=["Appetite?", "Vomiting?", "BM?", "Urine/Water intake?", "Coughing?", "Sneezing?"];
+
+const intakeQs=["Present Diet/Treats?", "Daily Medications/Supplements/Vitamins? (mg/mL, frequency)"];
+
+const prevQs=
+["Heartworm prevention?", "Last purchased?  How many?", "Last given?", "Flea/tick prevention?", "Last purchased?  How many?", "Last given?"];
+
+const felQ="Does Pet go outside?";
+
+const medProgQs=["Last Appointment?", "Progression?", "Any additional concerns?", "UTD on Wellness?"];
+
+const eyeQs=[];
+const earQs=[];
+const dermQs=[];
+const vomitQs=[];
+const diarQs=[];
+const vdQs=[];
+const urinQs=[];
+const coughQs=[];
+const limpQs=[];
+
+
+
